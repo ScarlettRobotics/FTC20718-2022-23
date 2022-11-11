3c80bd41b964d4c5e18ed5aeb8af04658ed2d495
Sam-Trepac <Sam.trepac@gmail.com>
2022-11-10 23:04:59 -0700

    minor changes... again... again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive/TankDrive.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
 public class TankDrive extends OpMode {
-    TriMotorDrive drive = new TriMotorDrive;
+    TriMotorDrive drive = new TriMotorDrive();
 
 
 

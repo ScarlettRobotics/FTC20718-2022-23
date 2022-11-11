cf7877f0a17370e413f1b7df4d7a87a05ca111b3
Sam-Trepac <Sam.trepac@gmail.com>
2022-11-10 23:36:42 -0700

    Added checking and power implementation to SlideCore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlideCore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core/SlideCore.java
@@ -10,4 +10,21 @@ public class SlideCore {
         slideMotor = hardwareMap.get(DcMotor.class, "slide_motor");
         slideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+
+    /** setSlidePower
+     *
+     *  Sets the power to the motor. Ensures it is between -1, and 1. Although this is made somewhat redundant
+     *  if one were to use hard-coded literals for the button maps (i.e if pushed then set value to 1); it
+     *  makes it easy to implement if it were attached to an analog input.
+     *
+     * @param slidePower Raw input power
+     */
+    public void setSlidePower(double slidePower){
+        double largest = 1.0;
+        largest = Math.max(largest, Math.abs(slidePower));
+
+        slideMotor.setPower((slidePower / largest));
+
+    }
+
 }

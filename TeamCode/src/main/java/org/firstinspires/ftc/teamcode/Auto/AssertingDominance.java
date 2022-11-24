e6612fcdefbe1785fde525592a8d5b4a62c2ba22
Daniel Liang <74940473+someone2060@users.noreply.github.com>
2022-11-24 16:47:02 -0700

    Attempt to fix funny auto
    funny auto hehehehaw

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AssertingDominance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/AssertingDominance.java
@@ -9,41 +9,44 @@ import org.firstinspires.ftc.teamcode.Core.SlideCore;
 
 @Autonomous(name = "AssertDominance", group = "Auto")
 public class AssertingDominance extends LinearOpMode {
-
+    // Initialize classes from other files
     private DualMotorDrive drive;
     private ClawCore claw;
     private SlideCore slide;
+
+    // Initialize variable for counting time passed from a set instance
     private ElapsedTime runtime = new ElapsedTime();
 
-    //Vars
-    //Motor Speeds
+    // Initialize double variables for each wheel motor
     private double leftMotor = 0;
     private double rightMotor = 0;
+
     @Override
     public void runOpMode() throws InterruptedException {
         telemetry.addData("ASSERTING DOMINANCE: ", "ACTIVE");
         telemetry.addData("FTC Team #", "20718");
         telemetry.update();
 
-        //Init
+        // Define classes from other files
         drive = new DualMotorDrive(hardwareMap);
         claw = new ClawCore(hardwareMap);
         slide = new SlideCore(hardwareMap);
 
+        // Continues to the next line when the program is initiated in the controller hub (?)
         waitForStart();
 
-        runtime.reset();
+        runtime.reset(); //reset timer
+        // Set the slide to fully move up for the duration of the auto period
+        slide.setSlidePower(1);
+        wait(2);
         while (opModeIsActive() && (runtime.seconds() <= 20)) {
-            slide.setSlidePower(1);
-            wait(5);
-            claw.clawOpen();
-            wait(1);
-            while(true){
-                claw.clawOpen();
-                wait(1);
-                claw.clawClose();
-            }
+            // Toggles the claw every 100 ms (open -> closed, or closed -> open)
+            claw.clawToggle();
+            wait(100);
         }
+        // Reset slide power
+        slide.setSlidePower(0);
+
         telemetry.update();
     }
 }

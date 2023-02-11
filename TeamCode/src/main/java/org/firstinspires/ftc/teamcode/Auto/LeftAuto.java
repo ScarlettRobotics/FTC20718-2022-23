c8c9832a18b07b5376e09ffaa5da12330427e5ed
aidandriscoll <aidan.driscoll@me.com>
2023-02-11 11:00:49 -0700

    Add OpenCV dependency, and introduce CV and Auto code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/LeftAuto.java
@@ -0,0 +1,108 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+/** MOVEMENTS TO MAKE:
+ * adjust to high junction
+ * strafe 7 in
+ * move 52 in forward
+ * strafe 12 in
+ * CASES:
+ *  1: strafe 12 in/36 in
+ *  2: strafe 12 in
+ *  3: strafe 36 in/12 in */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.teamcode.Core.CV.SleeveDetector;
+import org.firstinspires.ftc.teamcode.Core.CV.WebcamCore;
+import org.firstinspires.ftc.teamcode.Core.ClawCore;
+import org.firstinspires.ftc.teamcode.Core.DualMotorDrive;
+import org.firstinspires.ftc.teamcode.Core.SlideCore;
+
+@Autonomous(name="Left Auto", group="Auto")
+
+public class LeftAuto extends LinearOpMode {
+    private ElapsedTime runtime = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    protected DualMotorDrive drive;
+    protected ClawCore claw;
+    protected SlideCore slide;
+
+    protected WebcamCore webcam;
+    protected SleeveDetector sleeveDetector;
+
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        runtime.reset();
+
+        // Define classes
+        drive = new DualMotorDrive(hardwareMap);
+        claw = new ClawCore(hardwareMap);
+        slide = new SlideCore(hardwareMap);
+
+        webcam = new WebcamCore(hardwareMap);
+
+        sleeveDetector = new SleeveDetector();
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        //Stage 1: Scan and prepare for movements
+        int sleevePos = sleeveDetector.sleevePos();
+        waitForStart();
+
+        runtime.reset();
+        // TODO rightMotor moves before leftMotor; they should move at the same time
+        //drive.moveInches(40, 40, 0);
+        // strafe right to center on tile
+
+        // run until the end of match (driver pressed STOP)
+        while(opModeIsActive()) {
+            if ((int)(runtime.time() / 600) % 2 == 1) {
+                claw.close();
+            } else {
+                claw.open();
+            }
+
+            drive.update();
+            // Telemetry
+            telemetry.addData("FTC Team #", "22531");
+            telemetry.addData("Elapsed time", "%4.2f", runtime.time());
+            drive.telemetry(telemetry);
+            telemetry.addData("sleevePos", sleevePos);
+            telemetry.update();
+            //TODO
+        }
+        //proceed forward
+
+        //raise arm
+
+        //turn right to post
+
+        //forward to post
+
+        //slide down slightly
+
+        claw.open(); // To drop cone
+
+        // Raise claw
+
+        //Reverse
+
+        //Lower slide
+
+
+        //PARK
+
+        //switch state based on cam
+        //park 1
+
+        //park 2
+
+        //park 3
+
+        //fin - burn rest of time standing still
+
+
+    }
+}
